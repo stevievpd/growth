2fc182ab4e194a37f41a56802c3b7e565c698709
stevievpd <stevie.vpd@gmail.com>
2022-07-22 11:19:12 +0800

    save

---
+++ employee_delete.php
@@ -0,0 +1,18 @@
+<?php
+
+require('db/db.php');
+include('header.php');
+
+if(isset($_GET['deleteid'])){
+    $employee_id=$_GET['deleteid'];
+}
+    $sql="DELETE from employee WHERE employee_id=$employee_id";
+    $result=mysqli_query($con,$sql);
+    if($result){
+         header("location: employee-table.php");
+    }
+    else{
+         die(mysqli_error($con));
+    }    
+
+?>

e9b8a98dad34504f970cd2aaf0f3711ecf53be5f
sammy-vpd <sammy.vpd@gmail.com>
2022-07-22 16:53:22 +0800

    keysearch done

--- employee-table.php
+++ employee-table.php
@@ -48,76 +48,8 @@ if(isset($_SESSION['ROLE']) && $_SESSION['ROLE']!='1'){
 
                 </tr>
             </thead>
-            <tbody>
-
-                <?php
-
-    $sql = "SELECT * from employee";
-    $result=mysqli_query($con, $sql);
-     if($result){
-    while($row=mysqli_fetch_assoc($result))
-    {
-        $employee_id=$row['employee_id'];
-        $first_name=$row['first_name'];
-        $last_name=$row['last_name'];
-        $email=$row['email'];
-        $phone_number=$row['phone_number'];
-        $hire_date=$row['hire_date'];
-
-
-        echo ' <tr>
-        <th scope="row">'.$employee_id.'</th>
-        <td>'.$first_name.'</td>
-        <td>'.$last_name.'</td>
-        <td>'.$email.'</td> 
-        <td>'.$phone_number.'</td>
-        <td>'.$hire_date.'</td>
-
-        
-        <td>
-        <button class ="btn btn-primary my-1"><a href="employee-update.php?updateid= '.$employee_id.'" class = "text-light"><i class="fa-solid fa-file-pen"></i></a></button>
-		<button class ="btn btn-danger my-1"><a href="employee-delete.php?deleteid= '.$employee_id.'" class = "text-light"><i class="fa-solid fa-folder-minus"></i></a></button>
-        <button class ="btn btn-warning my-1"><a href="employee-view.php?viewid= '.$employee_id.'" class = "text-light"><i class="fa-solid fa-users-viewfinder"></i></a></button>
-
-        <!-- Button trigger modal -->
-		<button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteConfirm">
-            <i class="fa-solid fa-folder-minus"></i>
-		</button>
-
-		<!-- Modal -->
-		<div class="modal fade" id="deleteConfirm" tabindex="-1" role="dialog" aria-labelledby="deleteConfirmLabel" aria-hidden="true">
-		  <div class="modal-dialog" role="document">
-		    <div class="modal-content">
-		      <div class="modal-header">
-		        <h5 class="modal-title" id="deleteConfirmLabel">Warning!</h5>
-		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-		          <span aria-hidden="true">&times;</span>
-		        </button>
-		      </div>
-		      <div class="modal-body">
-                <p>Are you sure you want to delete '.$first_name.' '.$last_name.'?</p>
-
-		      </div>
-		      <div class="modal-footer">
-		        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-		        <button class="btn btn-danger my-1">
-				    <a href="employee-delete.php?deleteid= '.$employee_id.'">Yes</a>	
-				</button>
-		      </div>
-		    </div>
-		  </div>
-		</div>
-       
-      </td>
-	  
-      </tr>';
-    }
-    
-  }
-
-  ?>
-
-
+            <tbody id="output">
+                
             </tbody>
             
         </table>
@@ -127,7 +59,7 @@ if(isset($_SESSION['ROLE']) && $_SESSION['ROLE']!='1'){
 
     <script type="text/javascript">
     $(document).ready(function() {
-        $("#search").keypress(function() {
+        $("#search").keyup(function() {
             $.ajax({
                 type: 'POST',
                 url: 'search.php',
